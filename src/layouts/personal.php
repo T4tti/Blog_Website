ae35fbc0be562f7bed51c7369f3403f8daefa52f
Thanh Tai <taile.ngth@gamil.com>
2024-12-06 10:58:09 +0700

    rename: tittle

--- src/layouts/personal.php
+++ src/layouts/personal.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Quản lý tài khoản</title>
+  <title>VietTechBlog Accounts</title>
+    <link rel="icon" type="image/png" href="../Assets/favicon-32x32.png" sizes="32x32" />
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="../CSS/personal.css">
--- src/layouts/profile.php
+++ src/layouts/profile.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['username'])) {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Profile</title>
+  <title>Profile | VietTechBlog</title>
   <link rel="stylesheet" href="../CSS/profile.css" />
   <link rel="icon" type="image/png" href="../Assets/favicon-32x32.png" sizes="32x32" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
