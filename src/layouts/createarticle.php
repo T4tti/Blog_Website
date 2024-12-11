c22f1afed61d15a2cf38180a928e97b4c297935c
Thanh Tai <taile.ngth@gamil.com>
2024-12-11 21:33:56 +0700

    refactor: remove navbar code from createarticle.php and include navbar.php for better modularity

--- src/layouts/createarticle.php
+++ src/layouts/createarticle.php
@@ -24,53 +24,7 @@ require_once '../PHP/config.php';
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-    <div class="container">
-      <a class="navbar-brand" href="index.php">
-        <img src="../Assets/android-icon-48x48.png" alt="Logo" class="me-2" />
-        VietTechBlog
-      </a>
-
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="newest">Bài Viết</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="question">Hỏi Đáp</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="discussion">Thảo Luận</a>
-          </li>
-        </ul>
-
-        <!-- Kiểm tra trạng thái đăng nhập -->
-        <?php if (isset($_SESSION['username'])): ?>
-        <div class="dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuLink" data-bs-toggle="dropdown"
-            aria-expanded="false">
-            <img src="../Assets/icons8-avatar-24.png" alt="User" class="me-2" />
-            <?php echo htmlspecialchars($_SESSION['username']); ?>
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <li><a class="dropdown-item" href="../layouts/profile.php">Hồ sơ cá nhân</a></li>
-            <li><a class="dropdown-item" href="../layouts/createarticle.php">Viết bài</a></li>
-            <li><a class="dropdown-item text-danger" href="../PHP/logout.php">Đăng xuất</a></li>
-          </ul>
-        </div>
-        <?php else: ?>
-        <a class="nav-link" href="login.html" id="btn-login">
-          <img src="../Assets/icons8-login-24.png" alt="Login" class="me-2" />
-          Đăng nhập/Đăng ký
-        </a>
-        <?php endif; ?>
-      </div>
-    </div>
-  </nav>
+  <?php include 'navbar.php'  ?>
 
   <div class="editor-container">
     <input type="text" class="editor-input" placeholder="Tiêu đề" />
